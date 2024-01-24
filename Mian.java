5ef579f32fb7737009f4cd10c554f890bf371f6f
VANN YA <99462479+VannYa1212@users.noreply.github.com>
2024-01-24 23:47:24 +0700

    Delete unnecessary files and code

--- Mian.java
+++
@@ -1,9 +0,0 @@
-/**
- * Mian
- */
-public class Mian {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- README.md
+++
@@ -1,1 +0,0 @@
-# Employee Shift Scheduler
